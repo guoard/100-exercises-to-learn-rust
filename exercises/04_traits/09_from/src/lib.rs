bcd4c3205f0d7ab8f38a6bf16132eec2baf9c85e
Ali Afsharzadeh <afsharzadeh8@gmail.com>
2024-09-05 08:39:31 +0330

    04_traits

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -1,4 +1,9 @@
 // TODO: Implement the `From` trait for the `WrappingU32` type to make `example` compile.
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        Self { value }
+    }
+}
 
 pub struct WrappingU32 {
     value: u32,
